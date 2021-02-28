b755acd984d97ee0f08bde9257755480f17dbc2b
mjimem8 <4273880-mjimem8@user.noreply.gitlab.com>
2021-02-28 15:59:51 +0100

    quitar subrrayado enlace

--- src/pages/Main/components/newsBox.js
+++ src/pages/Main/components/newsBox.js
@@ -3,7 +3,7 @@ export const NewsBox = ({news}) => {
   return (
     <div className="col-md-4">
       <div className="fh5co-blog animate-box">
-        <a href="#" className="blog-bg" style={{backgroundImage:  `url(${news.photo})`}}></a>
+        <a className="blog-bg" style={{backgroundImage:  `url(${news.photo})`}}></a>
         <div className="blog-text">
           <span className="posted_on">{ news.date }</span>
           <h3>{ news.title }</h3>
